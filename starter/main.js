f840baff083213330c9837ee29e079cd4a563770
PrettyUserr <techbabyy@gmail.com>
2022-10-20 13:33:55 +0100

    added exciting javascript qualities

---
+++ starter/main.js
@@ -0,0 +1,26 @@
+ScrollReveal({ reset: true, duration: 3000, })
+
+ScrollReveal().reveal('.heading, .line' , {origin: 'left',
+distance: '100px',
+duration: 2000,});
+
+ScrollReveal().reveal('.logotext', { scale: 3, origin: 'right',
+distance: '800px',
+duration: 2500,});
+
+ScrollReveal().reveal('.firstsection-leftdiv p', {origin: 'right',
+distance: '100px',
+duration: 2000,});
+
+
+ScrollReveal().reveal(' .line' , {origin: bottom, delay : 1000}
+);
+
+
+ScrollReveal().reveal('.secondsection-leftdiv', { rotate: {
+    y: 180,
+    // y: -180,
+    // z: 0,
+},
+distance: '100px',
+duration: 3000,});
